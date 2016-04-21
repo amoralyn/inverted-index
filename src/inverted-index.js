a1abcc0859a9d9a49f461b958a636f8e4e1f9110
amoralyn <lovelyn.tijesunimi-israel@andela.com>
2016-04-21 20:10:23 +0100

    Refactor code

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,91 +1,99 @@
-/**
- * define Index class
- */
-var InvertedIndex = function () {};
+var InvertedIndex;
+(function() {
+  'use strict';
 
-//end of Index class
+  // Define Index class
+  InvertedIndex = function() {};
 
-/**
- * define readFile function
- */
-InvertedIndex.prototype.readFile = function (filePath) {
-  var content;
-  $.ajax({
-    url:filePath,
-    async:false,
-    dataType: 'json',
-    error: function(err) {
-      console.log("error");
-    },
-    success: function(data) {
-      content = data;
-    }
-  });
-  return content;
-};
-
-InvertedIndex.prototype.data = {};
-
-/**
- * define cleanUp function the replaces special characters in a string
- */
-InvertedIndex.prototype.cleanUp = function (wordsToBeCleaned){
-  return wordsToBeCleaned.replace(/[,.:;'"?!-#_*@%$&<>]/g,"");
-};
-
-InvertedIndex.prototype.createIndex = function (filePath) {
-  var that = this;
-  var jsonFile = this.readFile(filePath);
-  for (var bookIndex=0; bookIndex<jsonFile.length; bookIndex++) {
-    var bookContent = jsonFile[bookIndex].title + " " + jsonFile[bookIndex].text;
-    bookContent = this.cleanUp(bookContent);
-    bookContent = bookContent.toLowerCase();
-    var wordsInBook = bookContent.split(/\s/);
-    this.data[bookIndex] = wordsInBook;
-    for(var word in wordsInBook){
-      if (this.data[wordsInBook[word]]){
-        if(this.data[wordsInBook[word]].indexOf(bookIndex) === -1){
-          this.data[wordsInBook[word]].push(bookIndex);
+  InvertedIndex.prototype = {
+    // Define readFile function
+    readFile : function(filePath) {
+      var content;
+      $.ajax({
+        url:filePath,
+        async:false,
+        dataType: 'json',
+        error: function(err) {
+          console.log('Could not read JSON file');
+        },
+        success: function(data) {
+          content = data;
         }
-      } else{
-      this.data[wordsInBook[word]] = [bookIndex];
+      });
+      return content;
+    },
+    // End readFile function
+
+    data : {},
+
+    // Define cleanUp function to remove special characters
+    cleanUp : function(wordsToBeCleaned) {
+      return wordsToBeCleaned.replace(/[,.:;'"?!-#_*@%$&<>]/g,'');
+    },
+
+    // Define createIndex function
+    createIndex : function(filePath) {
+      var jsonFile = this.readFile(filePath);
+      var len = jsonFile.length;
+
+      /** Loops through the object to remove special characters,
+      * change to lowercase and split
+      */
+      for (var bookIndex = 0; bookIndex < len; bookIndex++) {
+        var bookContent = jsonFile[bookIndex].title + ' ' + jsonFile[bookIndex].text;
+        bookContent = this.cleanUp(bookContent).toLowerCase();
+        var wordsInBook = bookContent.split(/\s/);
+        this.data[bookIndex] = wordsInBook;
+
+        // Loops to check if word already exists in the index
+        for (var word in wordsInBook) {
+          if (this.data[wordsInBook[word]]) {
+            if (this.data[wordsInBook[word]].indexOf(bookIndex) === -1) {
+              this.data[wordsInBook[word]].push(bookIndex);
+            }
+          } else {
+              this.data[wordsInBook[word]] = [bookIndex];
+          }
+        }
+      }
+      return this.data;
+    },
+
+    // Define getIndex function
+    getIndex : function(filePath) {
+      this.createIndex(filePath);
+      return this.data[wordsInBook];
+    },
+
+    // Define searchIndex function
+    searchIndex : function(terms) {
+      var termsArray;
+      var that = this;
+      if (Array.isArray(terms)) {
+        termsArray = terms;
+        } else if(terms.indexOf(' ') ===-1) {
+          return checkOwnProperty(this.data, terms);
+        }
+        else {
+        terms = this.cleanUp(terms).toLowerCase();
+        termsArray = terms.split(/\s/);
+        }
+
+      var result =  termsArray.map(function(term) {
+         return checkOwnProperty(that.data, term);
+      });
+      return result;
+    },
+  };
+
+  // Define checkOwnProperty function to check if a term already exists
+  function checkOwnProperty(data, term) {
+    if (data.hasOwnProperty(term)) {
+      return data[term];
     }
+    else {
+      return [];
     }
   }
-};
-/**
- * define getIndex function
- */
-InvertedIndex.prototype.getIndex = function (filePath) {
-  this.createIndex(filePath);
-  return this.data[wordsInBook];
-};// end of getIndex function
 
-InvertedIndex.prototype.searchIndex = function (terms) {
-  var termsArray;
-  var that = this;
-  if(Array.isArray(terms)){
-    termsArray = terms;
-  } else if(terms.indexOf(' ') ===-1){
-      return checkOwnProperty(this.data , terms);
-    }
-    else{
-    terms = this.cleanUp(terms).toLowerCase();
-    termsArray = terms.split(/\s/);
-  }
-
-  var result =  termsArray.map(function(term){
-     return checkOwnProperty(that.data , term);
-  });
-
-  return result;
-}; //end searchIndex function
-
-function checkOwnProperty(data , term){
-  if(data.hasOwnProperty(term)){
-    return data[term];
-  }
-  else{
-    return [];
-  }
-}
+})();
