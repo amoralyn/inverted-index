7bb06d08e41a8daf7b4973fd0819aa82014f9c54
amoralyn <lovelyn.tijesunimi-israel@andela.com>
2016-04-22 02:07:03 +0100

    Add helper class

---
+++ src/inverted-index-helper.js
@@ -0,0 +1,56 @@
+function HelperMethods() {
+  // Cache
+  this._index = {};
+}
+
+// Define findIndex function to check if a term already exists
+HelperMethods.prototype.findIndex = function(data, term) {
+    return data.hasOwnProperty(term) ? data[term] : [];
+};
+
+// Define cleanUp function the replaces special characters in a string
+HelperMethods.prototype.cleanUp = function(wordsToBeCleaned) {
+    return wordsToBeCleaned.replace(/[^a-zA-Z\s]/g, '').toLowerCase();
+};
+
+// Define readFile function
+HelperMethods.prototype.readFile = function(filePath) {
+    var content = [];
+
+    $.ajax({
+        url: filePath,
+        async: false,
+        dataType: 'json',
+        error: function(err) {
+            console.log('Could not read JSON file');
+        },
+        success: function(data) {
+            content = data;
+        }
+    });
+
+    return content;
+};
+
+// Does the indexing of the word in the object
+HelperMethods.prototype.doIndexing = function(bookIndex, booksData) {
+    for (var word in booksData) {
+        if (this._index[booksData[word]]) {
+          if (!~this._index[booksData[word]].indexOf(bookIndex)){
+            this._index[booksData[word]].push(bookIndex);
+          }
+        } else {
+            this._index[booksData[word]] = [bookIndex];
+        }
+    }
+};
+
+HelperMethods.prototype.doSearch = function(index, terms) {
+    var mapFn = function(term) {
+        return helper.findIndex(index, term);
+    };
+
+    return terms.map(mapFn);
+};
+
+var helper = new HelperMethods();
