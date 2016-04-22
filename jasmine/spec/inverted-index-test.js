1ec97d6a2febd4d858f009aa5dc8c017f798f6e3
amoralyn <lovelyn.tijesunimi-israel@andela.com>
2016-04-22 02:08:15 +0100

    Refactor test cases

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -10,11 +10,12 @@
   'use strict';
 
   describe ('Inverted Index test cases', function () {
-    var index = new InvertedIndex();
+    var invertedIndex = new InvertedIndex();
+    var helper = new HelperMethods();
     var result;
 
     beforeEach(function(done) {
-      result = index.createIndex('books.json');
+      result = invertedIndex.createIndex('books.json');
        done();
       });
 
@@ -26,62 +27,62 @@
 
     describe ('Read book data', function () {
       it ('readFile method should not return empty JSON', function () {
-      var jsonFile = index.readFile('books.json');
+      var jsonFile = helper.readFile('books.json');
       expect (jsonFile.length).toBeGreaterThan(0);
       expect (jsonFile).not.toBe([]);
       });
 
     });
 
-    describe ('Populate index', function () {
-      it ('verifies that index is created once jsonFile is read', function () {
-      expect (index.data).toBeDefined();
+    describe ('Populate invertedIndex', function () {
+      it ('verifies that invertedIndex is created once jsonFile is read', function () {
+      expect (invertedIndex.index).toBeDefined();
       });
 
-      it ('verifies that index is correct', function () {
-      expect (index.data.alice).toEqual([0]);
-      expect (index.data.of).toEqual([0,1]);
-      expect (index.data.lord).toEqual([1]);
-      expect (index.data.wonderful).toBeUndefined();
+      it ('verifies that invertedIndex is correct', function () {
+      expect (invertedIndex.index.alice).toEqual([0]);
+      expect (invertedIndex.index.of).toEqual([0,1]);
+      expect (invertedIndex.index.lord).toEqual([1]);
+      expect (invertedIndex.index.wonderful).toBeUndefined();
       });
     });
 
-    describe ('Search index', function () {
+    describe ('Search invertedIndex', function () {
       it ('verifies that search returns correct result', function () {
-      expect (index.searchIndex('alice wonderland')).toEqual([[0],[0]]);
-      expect (index.searchIndex('lord ring of')).toEqual([[1],[1],[0,1]]);
-      expect (index.searchIndex('the')).toEqual([1]);
-      expect (index.searchIndex('they')).not.toEqual([1]);
+      expect (invertedIndex.searchIndex('alice wonderland')).toEqual([[0],[0]]);
+      expect (invertedIndex.searchIndex('lord ring of')).toEqual([[1],[1],[0,1]]);
+      expect (invertedIndex.searchIndex('the')).toEqual([1]);
+      expect (invertedIndex.searchIndex('they')).not.toEqual([1]);
       });
 
-      it('verifies searching an index returns array of indices of correct object', function() {
-      expect (index.searchIndex('into')).toEqual([0]);
-      expect (index.searchIndex('wizard')).toEqual([1]);
-      expect (index.searchIndex('a')).toEqual([0, 1]);
-      expect (index.searchIndex('of')).toEqual([0, 1]);
-      expect (index.searchIndex('lord')).toEqual([1]);
-      expect (index.searchIndex('elf')).toEqual([1]);
-      expect (index.searchIndex('elven')).not.toEqual([0]);
+      it('verifies searching an invertedIndex returns array of indices of correct object', function() {
+      expect (invertedIndex.searchIndex('into')).toEqual([0]);
+      expect (invertedIndex.searchIndex('wizard')).toEqual([1]);
+      expect (invertedIndex.searchIndex('a')).toEqual([0, 1]);
+      expect (invertedIndex.searchIndex('of')).toEqual([0, 1]);
+      expect (invertedIndex.searchIndex('lord')).toEqual([1]);
+      expect (invertedIndex.searchIndex('elf')).toEqual([1]);
+      expect (invertedIndex.searchIndex('elven')).not.toEqual([0]);
       });
 
       it('can handle varied number of search terms', function() {
-      expect(index.searchIndex('alice in wonderland')).toEqual([[0],[0],[0]]);
-      expect(index.searchIndex('lord of the rings')).toEqual([[1],[0, 1],[1],[1]]);
-      expect(index.searchIndex('an unusual alliance of man')).toEqual([[1],[1],[1],[0, 1],[1]]);
-      expect(index.searchIndex('me under allow if money')).not.toEqual([[1],[1],[1],[0, 1],[1]]);
+      expect(invertedIndex.searchIndex('alice in wonderland')).toEqual([[0],[0],[0]]);
+      expect(invertedIndex.searchIndex('lord of the rings')).toEqual([[1],[0, 1],[1],[1]]);
+      expect(invertedIndex.searchIndex('an unusual alliance of man')).toEqual([[1],[1],[1],[0, 1],[1]]);
+      expect(invertedIndex.searchIndex('me under allow if money')).not.toEqual([[1],[1],[1],[0, 1],[1]]);
       });
 
       it('ensures search can handle an array of words', function() {
-      expect(index.searchIndex(['alice', 'wonderland', 'in'])).toEqual([[0],[0],[0]]);
-      expect(index.searchIndex(['lord', 'of', 'the', 'rings'])).toEqual([[1],[0, 1],[1],[1]]);
-      expect(index.searchIndex(['an', 'unusual', 'alliance', 'of', 'man'])).toEqual([[1],[1],[1],[0, 1],[1]]);
-      expect(index.searchIndex(['me', 'under', 'allow', 'if', 'money'])).not.toEqual([[1],[1],[1],[0, 1],[1]]);
+      expect(invertedIndex.searchIndex(['alice', 'wonderland', 'in'])).toEqual([[0],[0],[0]]);
+      expect(invertedIndex.searchIndex(['lord', 'of', 'the', 'rings'])).toEqual([[1],[0, 1],[1],[1]]);
+      expect(invertedIndex.searchIndex(['an', 'unusual', 'alliance', 'of', 'man'])).toEqual([[1],[1],[1],[0, 1],[1]]);
+      expect(invertedIndex.searchIndex(['me', 'under', 'allow', 'if', 'money'])).not.toEqual([[1],[1],[1],[0, 1],[1]]);
       });
 
       it('ensures search can handle edge cases', function() {
-        expect(index.searchIndex(['alice', 'bicycle', 'in'])).toEqual([[0],[],[0]]);
-        expect(index.searchIndex(['alive', 'bicycle', 'on'])).toEqual([[],[],[]]);
-        expect(index.searchIndex(['alive', 'bicycle', 'on'])).not.toEqual([[0],[1],[1]]);
+        expect(invertedIndex.searchIndex(['alice', 'bicycle', 'in'])).toEqual([[0],[],[0]]);
+        expect(invertedIndex.searchIndex(['alive', 'bicycle', 'on'])).toEqual([[],[],[]]);
+        expect(invertedIndex.searchIndex(['alive', 'bicycle', 'on'])).not.toEqual([[0],[1],[1]]);
       });
   });
 
